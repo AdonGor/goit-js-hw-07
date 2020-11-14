f6d1e4be34745057b06696b2e06cdd100ea3ba05
Sergij <AdonGor@users.noreply.github.com>
2020-11-14 12:48:14 +0200

    ....

--- task1.js
+++ task1.js
@@ -1,26 +1,15 @@
-// const quantity = document.querySelectorAll('.item');
-// console.log(`В списке ${quantity.length} категории`);
-
-// quantity.forEach((element) => {
-//     const categories = element.querySelector('h2').innerHTML;
-//     const quantityCategories = element.querySelectorAll('li').length;
-  
-// console.log(`Категория: ${categories} 
-// Количество элементов: ${quantityCategories}`)  
-// });
-
 "use strict";
 
-const totalCategories = document.querySelectorAll(".item");
-console.log(`В списке ${totalCategories.length} категории.`);
+const quantity = document.querySelectorAll('.item');
+console.log(`В списке ${quantity.length} категории`);
 
-const categoriesArray = [...totalCategories]
-  .map(
-    categories => `Категория: ${categories.children[0].textContent}
-Количество элементов: ${categories.children[1].children.length}`
-  )
-  .join("\n");
-console.log(categoriesArray);
+quantity.forEach((element) => {
+    const categories = element.querySelector('h2').textContent;
+    const quantityCategories = element.querySelectorAll('li').length;
+  
+console.log(`Категория: ${categories} 
+Количество элементов: ${quantityCategories}`)  
+});
 
 
 
--- task2.js
+++ task2.js
@@ -1,50 +1,27 @@
-// const itemList = document.createElement('ul');
-// itemList.id = 'ingredients';
-// console.log(itemList);
-
-// const ingredients = [
-//   'Картошка',
-//   'Грибы',
-//   'Чеснок',
-//   'Помидоры',
-//   'Зелень',
-//   'Приправы',
-// ];
-
-// const potato = document.createElement('li');
-// potato.textContent = ingredients[0];
-// const muchrooms = document.createElement('li');
-// muchrooms.textContent = ingredients[1];
-// const garlic = document.createElement('li');
-// garlic.textContent = ingredients[2];
-// const tomato = document.createElement('li');
-// tomato.textContent = ingredients[3];
-// const greens = document.createElement('li');
-// greens.textContent = ingredients[4];
-// const spice = document.createElement('li');
-// spice.textContent = ingredients[5];
-
-// itemList.append(potato, muchrooms, garlic, tomato, greens, spice);
-
-"use strict";
+'use strict';
 
 const ingredients = [
-  "Картошка",
-  "Грибы",
-  "Чеснок",
-  "Помидоры",
-  "Зелень",
-  "Приправы"
+  'Картошка',
+  'Грибы',
+  'Чеснок',
+  'Помидоры',
+  'Зелень',
+  'Приправы',
 ];
-let ulList = document.getElementById("ingredients");
+
+const itemList = document.createElement('ul');
+itemList.id = 'ingredients';
+
 const foodIngredients = ingredients.forEach(ingredient => {
-  let items = document.createElement("li");
-  items.innerHTML = ingredient;
-  ulList.append(items);
-  parent.append(...items)
+
+  let itemsProduct = document.createElement('li');
+
+  itemsProduct.textContent = ingredient;
+  itemList.append(itemsProduct);
+
 });
 
-console.log(foodIngredients);
+console.log(itemList);
 
 
 
